089a7cc296cf123198781c575936aa2a45c333fc
Steen Ole Andersen <soa@letpension.dk>
2022-11-30 08:20:06 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import DxButton from 'devextreme-vue/button';
+
+function sayHelloWorld() {
+  alert('Hello world!')
+}
+
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+      <DxButton
+          type="default"
+          text="Click me"
+          @click="sayHelloWorld"
+      />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import './assets/main.css'
+import 'devextreme/dist/css/dx.light.css';
+
+createApp(App).mount('#app')
